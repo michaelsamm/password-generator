7ce07bfbe68b41c198a7db974920e33cc7fafabe
msamm <msamm93@gmail.com>
2022-03-18 20:44:05 -0500

    commented out code and removed consolelogs

--- script.js
+++ script.js
@@ -1,60 +1,68 @@
-// Assignment code here
-
+// Character set variables and password length
 var passwordLength 
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "0123456789";
 var special = "!@#$%^&*()-+<>?|/"
 
+// Charactype type inclusion variables
 var lowercaseConfirm
 var uppercaseConfirm
 var numericConfirm
 var specialCharConfirm
 
+// Variable storing characters to use in password
 var passwordChars = ""
 
 
+// Function to prompt for password length
 var lengthPrompt = function() {
   var length = prompt("How many characters in the password?");
+  // Validate password length is between 8 and 128 characters
   while (length < 8 || length > 128) {
     length = prompt('Entered value must be at least 8 characters and no more than 128 characters.');
   }
+  // Set the global variable for password length
   passwordLength = length;
 }
 
+// Function to check for lowercase character inclusion
 var lowercaseChar = function() {
   lowercaseConfirm = confirm("Select 'Ok' to include lowercase characters in the password.");
+  // Add lowercase characters to passwordChars list
   if (lowercaseConfirm) {
     passwordChars = passwordChars.concat(lowercase);
   }
-  console.log(passwordChars);
 }
 
+// Function to check for uppercase character inclusion
 var uppercaseChar = function() {
   uppercaseConfirm = confirm("Select 'Ok' to include uppercase characters in the password.");
+  // Add uppercase characters to passwordChars list
   if (uppercaseConfirm) {
     passwordChars = passwordChars.concat(uppercase);
   }
-  console.log(passwordChars);
 }
 
+// Function to check for numeric character inclusion
 var numericChar = function() {
   numericConfirm = confirm("Select 'Ok' to include numeric characters in the password.");
+  // Add numeric characters to passwordChars list
   if (numericConfirm) {
     passwordChars = passwordChars.concat(numeric);
   }
-  console.log(passwordChars);
 }
 
+// Function to check for special character inclusion
 var specialChar = function() {
   specialCharConfirm = confirm("Select 'Ok' to include special characters in the password.");
+  // Add special characters to passwordChars list
   if (specialCharConfirm) {
     passwordChars = passwordChars.concat(special);
   }
-  console.log(passwordChars);
 }
 
-
+// Generate password 
 function generatePassword() {
   lengthPrompt();
   lowercaseChar();
@@ -62,24 +70,26 @@ function generatePassword() {
   numericChar();
   specialChar();
 
+  // Validate that at least one set of characters has been selected
   while (!lowercaseConfirm && !uppercaseConfirm && !numericConfirm && !specialCharConfirm) {
     alert("You must select at least one character type.");
-    passwordChars = "";
+    // Reprompt user for character types
     lowercaseChar();
     uppercaseChar();
     numericChar();
     specialChar();
-    
-    console.log(passwordChars);
   };
 
+  // Variable to generate password into
   var randomPassword = "";
 
+  // For the passwordLength selected, randomly select characters from passwordChars
   for (i = 0; i < passwordLength; i++) {
     randomPassword += passwordChars.charAt(Math.floor(Math.random() * passwordChars.length));
   }
-  return randomPassword;
 
+  // Return the generate password as the function value
+  return randomPassword;
 }
 
 // Get references to the #generate element
@@ -92,6 +102,7 @@ function writePassword() {
 
   passwordText.value = password;
 
+  // Reset passwordChars for next generation
   passwordChars = "";
 
 }
